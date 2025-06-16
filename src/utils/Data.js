062a06bd3bfd0625987284d3062e01d41f53e9b9
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2025-06-16 20:57:22 +0530

    Projec added codesensei

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -9,6 +9,7 @@ import Project1 from "../assets/Project1.png"
 import Project2 from "../assets/Project2.png"
 import Project3 from "../assets/Project3.png"
 import Project4 from "../assets/Project4.jpg"
+import Project_Codesensei from "../assets/Project_Codesensei.png"
 import { TiHtml5 } from "react-icons/ti";
 import { BsFiletypeSql } from "react-icons/bs";
 import { SiPytorch } from 'react-icons/si';
@@ -87,7 +88,7 @@ export const projectDetails = [
 
     {
         id: 0,
-        Project_name:"Crowd Control and Management using Deep Learning",
+        Project_name:"Crowd Control and Management using DL",
         Project_desc: " A fine YOLO model was fine tuned to track people in input video source, 2d scenes were converted into overhead 3d scene using automated homography and a heatmap is generated to detect violations. This helps and aids authorities to track and manage crowds at various public spaces. The system was tested using live video source from public spaces at college campus.",
         tech_stack: ["Pytorch","Python"],
         project_img: Project1,
@@ -107,6 +108,16 @@ export const projectDetails = [
 },
 {
     id:2,
+    Project_name:"Code Sensei",
+    Project_desc: "An AI-powered interview platform for Computer Science students to practice for technical Interviews. An AI agent powered by the MCP server generates an assessment for the users based on the input prompt and the previous performance of the user. LLM to provide constrained, context-aware hints as a helper during the interview process. Code execution and compiler services were implemented using ECS with Fargate to scale based on demand for 10,000+ consecutive users. Cached performance and prompts to reduce latency and calls to Bedrock LLM. ",
+    tech_stack:["AWS","EKS","Open Search","MCP","TypeScript"],
+    project_img: Project_Codesensei,
+    project_url: "https://github.com/JANARDHANAREDDYMS/CodeSensei",
+    video_url: "https://www.youtube.com/watch?v=T0FAQEtJMJY",
+    reverse: false,
+},
+{
+    id:3,
     Project_name:"Sephora Recommendation System",
     Project_desc: "Implemented a deep learning model with multilayer neural architecture using TensorFlow/Keras, including embedding layers and batch normalization, to recommend Sephora products based on user attributes like skin, hair, and product preferences, using an open-source dataset of Sephora sales, products, and reviews, while also considering price consciousness, with positive user testing feedback from college students. ",
     tech_stack:["Python","Keras","PyTorch"],
@@ -116,7 +127,7 @@ export const projectDetails = [
     reverse: false,
 },
 {
-    id: 3,
+    id: 4,
     Project_name:"Automated Lane Detection System",
     Project_desc: "An automated lane detection system using open source libraries like OpenCV,NumPy,Pandas. The system uses the principles of Hough Transform for lane detection using up vote technique",
     tech_stack: ["Python","OpenCv"],
